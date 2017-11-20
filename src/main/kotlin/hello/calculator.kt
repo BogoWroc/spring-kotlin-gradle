9b8f738769e7eb35f09231d593bf850cda3e4627
Bogumil Zebek <bogowrocpl@gmail.com>
2017-11-20 18:14:01 +0100

    Create separate module for calculator

---
+++ src/main/kotlin/hello/calculator.kt
@@ -0,0 +1,6 @@
+package hello
+
+class SampleCalculator() {
+    fun sum(a: Int, b: Int): Int = a + b
+    fun subtract(a: Int, b: Int): Int = a - b
+}
--- src/main/kotlin/hello/mix.kt
+++ src/main/kotlin/hello/mix.kt
@@ -5,5 +5,4 @@ package hello
  */
 
 data class Greeting(val id: Long, val content: String) {}
-class SampleCalculator(){}
 
